12a89a49c23e097feba63d93be020106e6603628
Mates Vrba <matesvrba@protonmail.com>
2022-01-15 11:11:54 +0100

    Basics of OpenGL rendering

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,41 @@
-mod font;
-mod renderer;
 mod window;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::rect::{Point, Rect};
-use std::time::Duration;
 
-fn main() -> Result<(), String> {
-    let mut win = window::init(1920, 1080)?;
-    let mut event_pump = win.context.event_pump()?;
-    let font_cont = font::init()?;
-    let font = font::load_font(&font_cont, "./TerminusTTF.ttf", 12)?;
+//use std::{time::Duration, fmt::Result};
 
-    let col = renderer::RGB {
-        r: 24,
-        g: 26,
-        b: 27,
+fn main() {
+    match sietor() {
+        Ok(it) => it,
+        Err(err) => panic!("{}", err),
     };
-    let mut r = 250;
-    let mut g = 250;
-    let mut b = 250;
-    let mut buffer = String::new();
-    'run: loop {
-        renderer::fill(&mut win, &col);
-        //        win.canvas.from_surface(font.font.render("asdf")):
-        if buffer.chars().count() > 0 {
-            let surf = match font
-                .font
-                .render(buffer.as_str())
-                .blended(sdl2::pixels::Color { r, g, b, a: 255 })
-            {
-                Ok(srf) => srf,
-                Err(error) => panic!("{}", error),
-            };
-            let tc = win.canvas.texture_creator();
-            let screen_position = Point::new((surf.width() / 2) as i32, (surf.height() / 2) as i32);
-            let screen_rect = Rect::from_center(screen_position, surf.width(), surf.height());
-            match win
-                .canvas
-                .copy(&surf.as_texture(&tc).unwrap(), None, screen_rect)
-            {
-                Ok(_) => {}
-                Err(error) => {
-                    panic!("{}", error);
-                }
-            };
-        }
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => {
-                    break 'run;
-                }
-                Event::KeyDown { keycode, .. } => match keycode {
-                    Some(kc) => {
-                        if kc as i32 >= Keycode::A as i32 && kc as i32 <= Keycode::Z as i32 {
-                            buffer.push_str(kc.to_string().as_str());
-                        }
-                        match kc {
-                            Keycode::Backspace => {
-                                buffer.pop();
-                            }
-                            Keycode::Space => {
-                                buffer.push(' ');
-                            }
-                            Keycode::Kp7 => {
-                                if r < 255 {
-                                    r = r + 5;
-                                }
-                            }
-                            Keycode::Kp8 => {
-                                if g < 255 {
-                                    g = g + 5;
-                                }
-                            }
-                            Keycode::Kp9 => {
-                                if b < 255 {
-                                    b = b + 5;
-                                }
-                            }
-                            Keycode::Kp4 => {
-                                if r > 0 {
-                                    r = r - 5;
-                                }
-                            }
-                            Keycode::Kp5 => {
-                                if g > 0 {
-                                    g = g - 5;
-                                }
-                            }
-                            Keycode::Kp6 => {
-                                if b > 0 {
-                                    b = b - 5;
-                                }
-                            }
-                            _ => {}
-                        }
-                    }
-                    None => {}
-                },
-                _ => {}
-            }
-        }
-        win.canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
+    //    let mut event_pump = win.window.event_pump()?;
+    //    let font_cont = font::init()?;
+    //        let font = font::load_font(&font_cont, "./TerminusTTF.ttf", 14)?;
+    //let font = font::load_font(&font_cont, "./SourceCodePro-Regular.otf", 12)?;
+
+    //    let col = renderer::RGB {
+    //        r: 24,
+    //        g: 26,
+    //        b: 27,
+    //    };
+    //    let mut buffer = String::new();
+    //    'run: loop {
+    //        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    //    }
+}
+
+fn sietor() -> Result<(), String> {
+    let win = window::WindowContext::new()?;
+
+    win.run()?;
+    //    loop {
+    //        match win.draw() {
+    //            Ok(it) => {
+    //                if !it {
+    //                    break;
+    //                }
+    //            }
+    //            Err(err) => return Err(err),
+    //        };
+    //    }
     Ok(())
 }
--- src/window.rs
+++ src/window.rs
@@ -1,24 +1,153 @@
-pub struct Window {
-    pub context: sdl2::Sdl,
-    pub canvas: sdl2::render::Canvas<sdl2::video::Window>,
+#![allow(dead_code)]
+use std::ffi::CString;
+
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::{ContextWrapper, PossiblyCurrent};
+extern crate gl;
+
+pub struct WindowContext {
+    event_loop: EventLoop<()>,
+    renderer: Renderer,
 }
 
-pub fn init(width: u32, height: u32) -> Result<Window, String> {
-    let context = sdl2::init()?;
-    let video_sub = context.video()?;
+pub struct Renderer {
+    pub window_context: ContextWrapper<PossiblyCurrent, glutin::window::Window>,
+    shaders: Vec<gl::types::GLuint>,
+}
+pub struct Shader {
+    id: gl::types::GLuint,
+}
 
-    let win = match video_sub
-        .window("sietor", width, height)
-        .position_centered()
-        .build()
-    {
-        Ok(win) => win,
-        Err(error) => return Err(error.to_string()),
-    };
-    let canvas = match win.into_canvas().build() {
-        Ok(canvas) => canvas,
-        Err(error) => return Err(error.to_string()),
-    };
-    let window = Window { context, canvas };
-    Ok(window)
+impl Renderer {
+    pub fn new(
+        context: ContextWrapper<PossiblyCurrent, glutin::window::Window>,
+    ) -> Result<Self, String> {
+        Ok(Renderer {
+            window_context: context,
+            shaders: Vec::new(),
+        })
+    }
+    pub fn draw(&self) {
+        unsafe {
+            gl::ClearColor(25.0 / 255.0, 27.0 / 255.0, 28.0 / 255.0, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        self.window_context.swap_buffers().unwrap();
+    }
+}
+
+impl Shader {
+    /// Compile shader from source
+    /// @param source - shader source
+    /// @param kind - shader type veretex/fragment
+    fn compile_shader(
+        source: &std::ffi::CStr,
+        kind: gl::types::GLuint,
+    ) -> Result<gl::types::GLuint, String> {
+        let id = unsafe { gl::CreateShader(kind) };
+        unsafe {
+            gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
+            gl::CompileShader(id);
+        }
+        let mut suecess: gl::types::GLint = 1;
+        unsafe {
+            gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut suecess);
+        }
+        if suecess == 0 {
+            let mut len: gl::types::GLint = 0;
+            unsafe {
+                gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+            }
+            let error = create_whitespace_cstring_with_len(len as usize);
+            unsafe {
+                gl::GetShaderInfoLog(
+                    id,
+                    len,
+                    std::ptr::null_mut(),
+                    error.as_ptr() as *mut gl::types::GLchar,
+                );
+            }
+            return Err(error.to_string_lossy().into_owned());
+        }
+
+        //TODO manage shaders in renderer?
+        //self.shaders.push(id);
+        Ok(id)
+    }
+
+    pub fn from_source(source: &std::ffi::CStr, kind: gl::types::GLenum) -> Result<Shader, String> {
+        let id = Shader::compile_shader(source, kind)?;
+        Ok(Shader { id })
+    }
+    pub fn vert_from_source(source: &std::ffi::CStr) -> Result<Shader, String> {
+        Shader::from_source(source, gl::VERTEX_SHADER)
+    }
+    pub fn frag_from_source(source: &std::ffi::CStr) -> Result<Shader, String> {
+        Shader::from_source(source, gl::FRAGMENT_SHADER)
+    }
+}
+
+impl Drop for Shader {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteShader(self.id);
+						self.id = 0;
+        }
+    }
+}
+
+fn create_whitespace_cstring_with_len(len: usize) -> CString {
+    // allocate buffer of correct size
+    let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
+    // fill it with len spaces
+    buffer.extend([b' '].iter().cycle().take(len));
+    // convert buffer to CString
+    unsafe { CString::from_vec_unchecked(buffer) }
+}
+
+impl WindowContext {
+    pub fn new() -> Result<Self, String> {
+        let event_loop = glutin::event_loop::EventLoop::new();
+        let window_builder = glutin::window::WindowBuilder::new()
+            .with_title("Hello world!")
+            .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
+
+        let windowed_context = glutin::ContextBuilder::new()
+            .build_windowed(window_builder, &event_loop)
+            .unwrap();
+
+        let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+        gl::load_with(|ptr| windowed_context.context().get_proc_address(ptr) as *const _);
+
+        let win = WindowContext {
+            event_loop,
+            renderer: Renderer::new(windowed_context)?,
+        };
+
+        Ok(win)
+    }
+    pub fn run(self) -> Result<(), String> {
+        self.event_loop.run(move |event, _, control_flow| {
+            println!("{:?}", event);
+            *control_flow = ControlFlow::Wait;
+
+            match event {
+                Event::LoopDestroyed => return,
+                Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::Resized(physical_size) => {
+                        self.renderer.window_context.resize(physical_size)
+                    }
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    _ => (),
+                },
+                Event::RedrawRequested(_) => {
+                    self.renderer.draw();
+                    self.renderer.window_context.swap_buffers().unwrap();
+                }
+                _ => (),
+            }
+        });
+        // Ok(())
+    }
 }
